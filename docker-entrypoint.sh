aad8c30041f0ecca30687b94d26f0a6cc380f647
iskhak <zevs0975@gmail.com>
2026-08-19 23:07:14 +0600

    fix: enforce linux entrypoint line endings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
+# Named volumes are created as root and must be prepared before dropping privileges.
 if [ "$(id -u)" = "0" ]; then
     mkdir -p /app/media /app/private_media
     chown -R app:app /app/media /app/private_media
